257c82208fb6d0bbe256d2fc87304fc8369357a2
Raktim Shrestha <rakimsth@gmail.com>
2025-02-10 16:52:44 +0545

    learned about variables

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
-console.log("hello");
+console.log("Hello World"); // print the message
 
-const person = "raktim";
+// ES5
+var name = "raktim"; // variable declare
+var name = "shyam"; // override
+console.log(name);
+
+// ES6
+// let, const
+
+const person = "raktim"; // no override
 console.log(person);
 
-let person1 = "sadip";
-console.log(person1);
+let school = "abc"; // allows override
+school = "xyz";
+console.log(school);
 
-var person2 = "saugat";
-console.log(person2);
+///////////////
+
+alert("hello world");
+
+// const user = prompt("What is your name?");
+// console.log(user);
+
+// const isStudent = confirm("are you above 18?");
+// console.log(isStudent);
+
+// Write a program that asks user for their name and alerts their name
+// with greeting
+
+const username = prompt("What is your name?");
+//1
+const greeter = "Welcome back," + username;
+alert(greeter);
+
+//2 String Literal
+alert(`Welcome back, ${username}`);
